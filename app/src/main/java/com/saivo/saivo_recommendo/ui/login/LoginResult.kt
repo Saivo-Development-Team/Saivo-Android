3feb5b0a0ae3c437f12f338f960acda4731decee
Ian Mubangizi <io@ianmubangizi.com>
2019-07-22 13:26:58 +0200

    Started Android Project

---
+++ app/src/main/java/com/saivo/saivo_recommendo/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.saivo.saivo_recommendo.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
