3c8b80745bd2a68d508c8b2893518e94c05fdea6
Ian Mubangizi <io@ianmubangizi.com>
2019-07-24 11:45:29 +0200

    Basic Android Set Up Boiler

--- app/src/main/java/com/saivo/saivo_recommendo/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.saivo.saivo_recommendo.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
