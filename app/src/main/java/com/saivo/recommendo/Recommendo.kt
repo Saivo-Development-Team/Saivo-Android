bbf0e79b2a7c0a2e8e4dacc8ddfc565fa9f6725a
Ian Mubangizi <io@ianmubangizi.com>
2019-08-19 00:36:01 +0200

    Added New Kodin Bindings for network services

--- app/src/main/java/com/saivo/recommendo/Recommendo.kt
+++ app/src/main/java/com/saivo/recommendo/Recommendo.kt
@@ -3,7 +3,12 @@ package com.saivo.recommendo
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.saivo.recommendo.data.Database
-import com.saivo.recommendo.network.Service
+import com.saivo.recommendo.data.repository.UserRepository
+import com.saivo.recommendo.data.repository.UserRepositoryImpl
+import com.saivo.recommendo.network.NetworkService
+import com.saivo.recommendo.network.access.DataSource
+import com.saivo.recommendo.network.access.NetworkDataSource
+import com.saivo.recommendo.view.viewmodel.UserViewModelFactory
 import com.saivo.recommendo.util.network.Connection
 import com.saivo.recommendo.util.network.Connectivity
 import kotlinx.coroutines.InternalCoroutinesApi
@@ -12,6 +17,7 @@ import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 class Recommendo : Application(), KodeinAware {
@@ -20,11 +26,13 @@ class Recommendo : Application(), KodeinAware {
         import(androidXModule(this@Recommendo))
 
         bind() from singleton { Database(instance()) }
-        bind() from singleton { Service(instance()) }
         bind() from singleton { instance<Database>().userDao() }
-        bind() from singleton { instance<Database>().clientDap() }
-
+        bind() from singleton { instance<Database>().clientDao() }
         bind<Connectivity>() with singleton { Connection(instance()) }
+        bind() from singleton { NetworkService(instance()) }
+        bind<DataSource>() with singleton { NetworkDataSource(instance()) }
+        bind<UserRepository>() with singleton { UserRepositoryImpl(instance(), instance()) }
+        bind() from provider { UserViewModelFactory(instance()) }
     }
 
     override fun onCreate() {
