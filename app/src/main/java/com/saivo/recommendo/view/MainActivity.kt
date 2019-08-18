9848f2cc814c054c5ea6c624302f2efbc642d196
Ian Mubangizi <io@ianmubangizi.com>
2019-08-19 00:46:09 +0200

    Ui Package moved to view

--- app/src/main/java/com/saivo/recommendo/view/MainActivity.kt
+++ app/src/main/java/com/saivo/recommendo/view/MainActivity.kt
@@ -1,11 +1,10 @@
-package com.saivo.recommendo.ui
+package com.saivo.recommendo.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.saivo.recommendo.R
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
