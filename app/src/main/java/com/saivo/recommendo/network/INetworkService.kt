08743711524dbbd79995eea0c4e3a8adb41f7e6f
Ian Mubangizi <io@ianmubangizi.com>
2019-09-07 09:13:17 +0200

    Removing API Keys From Code

--- app/src/main/java/com/saivo/recommendo/network/INetworkService.kt
+++ app/src/main/java/com/saivo/recommendo/network/INetworkService.kt
@@ -1,28 +1,16 @@
 package com.saivo.recommendo.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.saivo.recommendo.data.access.TokenDao
-import com.saivo.recommendo.data.repository.token.ITokenRepository
-import com.saivo.recommendo.util.DEVELOPMENT_URL
-import com.saivo.recommendo.util.DEVELOPMENT_URL_WIFI
-import com.saivo.recommendo.util.STAGGING_URL
+import com.saivo.recommendo.BuildConfig
 import com.saivo.recommendo.util.network.IConnection
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-/*
-* TODO:  - Find a better way to store this info
-*        - AUTHORIZATION_TOKEN expires, prevent HttpException: 401, 500 etc..
-*        - BASE_URL has to run on SSL
-* */
-
 interface INetworkService {
 
     companion object {
-
-        private lateinit var authorizationHeader: String
 
         inline operator fun <reified T> invoke(
             connection: IConnection
@@ -30,7 +18,7 @@ interface INetworkService {
             val interceptor = Interceptor {
                 val url = it.request().url().newBuilder().build()
                 val request = it.request().newBuilder().url(url)
-                    .addHeader("Authorization", "Bearer 294b14ec-9c1b-47d4-815b-c763ab005408")
+                    .addHeader("Authorization", BuildConfig.BearerToken)
                     .build()
                 return@Interceptor it.proceed(request)
             }
@@ -40,27 +28,10 @@ interface INetworkService {
                 .addInterceptor(connection)
                 .build()
 
-            return Retrofit.Builder().client(httpClient).baseUrl(STAGGING_URL)
+            return Retrofit.Builder().client(httpClient).baseUrl(BuildConfig.BaseUrl)
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build().create(T::class.java)
         }
-
-//        fun getHeaderValues(tokenDao: TokenDao): String {
-//            if (::authorizationHeader.isInitialized) {
-//                return authorizationHeader
-//            }
-//
-//            setHeaderValues(type = tokenDao.getTokenType(), authorization = tokenDao.getAccessToken())
-//            return authorizationHeader
-//        }
-//
-//        private fun setHeaderValues(type: String = "", authorization: String) {
-//            authorizationHeader = if (type.isNotBlank()) {
-//                "$type $authorization"
-//            } else {
-//                "Bearer $authorization"
-//            }
-//        }
     }
 }
--- app/src/main/java/com/saivo/recommendo/util/Values.kt
+++
@@ -1,8 +0,0 @@
-package com.saivo.recommendo.util
-
-
-const val MAPBOX_KEY = "pk.eyJ1IjoiaWFubXViYW5naXppIiwiYSI6ImNqemN0bWc2czAwMWkzanF0d3AzMWN2eDYifQ.ZAmKWLtL0Kzdsqqu_jGHQg"
-const val STAGGING_URL = "https://saivo-recommendo-stag.herokuapp.com/"
-const val PRODUCTION_URL = "https://saivo-recommendo.herokuapp.com/"
-const val DEVELOPMENT_URL = "http://10.0.2.2:8080"
-const val DEVELOPMENT_URL_WIFI = "http://192.168.137.1:8080"
