8d51b3bb8c8e199b56caafdbf8c8e294c22f58fb
Ian Mubangizi <io@ianmubangizi.com>
2019-09-07 14:54:27 +0200

    Connection passes cause and message stopping nulls

--- app/src/main/java/com/saivo/recommendo/util/exception/ConnectionOfflineException.kt
+++ app/src/main/java/com/saivo/recommendo/util/exception/ConnectionOfflineException.kt
@@ -2,4 +2,10 @@ package com.saivo.recommendo.util.exception
 
 import java.io.IOException
 
-class ConnectionOfflineException : IOException()
+class ConnectionOfflineException : IOException() {
+
+    override val cause: Throwable?
+        get() = super.cause?:this
+    override val message: String?
+        get() = super.message?:"Connection Offline"
+}
--- app/src/main/java/com/saivo/recommendo/util/network/Connection.kt
+++ app/src/main/java/com/saivo/recommendo/util/network/Connection.kt
@@ -2,25 +2,36 @@ package com.saivo.recommendo.util.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkRequest
 import com.saivo.recommendo.util.exception.ConnectionOfflineException
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class Connection(context: Context) : IConnection {
+
     private val applicationContext = context.applicationContext
     override fun intercept(chain: Interceptor.Chain): Response {
-        if (!gotInternet()){
-            throw ConnectionOfflineException()
-        }
+        if (!gotInternet()) throw ConnectionOfflineException()
         return chain.proceed(chain.request())
     }
 
     private fun gotInternet(): Boolean {
+        var connected: Boolean? = null
         val connectivityManager = applicationContext.getSystemService(
-                Context.CONNECTIVITY_SERVICE
+            Context.CONNECTIVITY_SERVICE
         ) as ConnectivityManager
-        val nesting = connectivityManager.activeNetworkInfo // TODO - Read Network Access API Docs - This may not be needed
-        return (nesting != null && nesting.isConnected) || connectivityManager.isDefaultNetworkActive // TODO - Read API
+
+        connectivityManager.registerNetworkCallback(
+            NetworkRequest.Builder().build(),
+            object : NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    connected = true
+                    super.onAvailable(network)
+                }
+            })
+        return connected ?: false
     }
 
 }
