4bc438ed80bd8d31937d449305ad5e87089ea989
Daniel Aaron <danielaaron7@gmail.com>
2019-09-24 19:32:42 +0200

    RecyclerView Setup

--- app/src/main/java/com/saivo/recommendo/view/main/page/PreferencesFragment.kt
+++ app/src/main/java/com/saivo/recommendo/view/main/page/PreferencesFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.saivo.recommendo.R
+import kotlinx.android.synthetic.main.fragment_preferences.*
+import kotlinx.android.synthetic.main.fragment_preferences.view.*
+
 
 class PreferencesFragment : Fragment() {
+
+    private lateinit var recyclerAdapter: RecyclerViewAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +24,46 @@ class PreferencesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_preferences, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        addDataSet()
+
+
+        //code for when Add Button is clicked
+        add_button.setOnClickListener{
+        var input = preference_input
+
+        }
+
+        //code for when Remove Button is clicked
+        remove_button.setOnClickListener{
+
+        }
+
+
+
+
     }
-}
+
+    private fun addDataSet(){
+        val data = DataSource.createDataSet()
+        recyclerAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView(){
+        recycler_preferences.apply {
+            recycler_preferences.layoutManager = LinearLayoutManager(context)  // May need to be @preferencesfragment
+            recyclerAdapter = RecyclerViewAdapter()
+            adapter = recyclerAdapter
+        }
+
+    }
+
+    }
+
+
+
+
