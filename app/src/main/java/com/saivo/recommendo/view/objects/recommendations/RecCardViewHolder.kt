c236e95b7d19b9c23eb475d8b9ca3ddedce32b05
Ian Mubangizi <io@ianmubangizi.com>
2019-09-26 10:56:04 +0200

    Add IRecyclerItem interface and with RecommendationCard/ViewHolder class

--- app/src/main/java/com/saivo/recommendo/view/objects/recommendations/RecCardViewHolder.kt
+++ app/src/main/java/com/saivo/recommendo/view/objects/recommendations/RecCardViewHolder.kt
@@ -1,13 +1,15 @@
-package com.saivo.recommendo.view.`object`
+package com.saivo.recommendo.view.objects.recommendations
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.saivo.recommendo.R
 import kotlinx.android.synthetic.main.recommendation_card.view.*
 
-class RecoCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class RecCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val info = itemView.info!!
     private val users = itemView.users
     private val title = itemView.title
@@ -19,7 +21,17 @@ class RecoCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val distance = itemView.distance
     private val category = itemView.category
 
-    fun bind(card: RecoCard) {
+    companion object {
+        operator fun invoke(root: ViewGroup): RecyclerView.ViewHolder {
+            return RecCardViewHolder(
+                LayoutInflater.from(root.context).inflate(
+                    R.layout.recommendation_card, root, false
+                )
+            )
+        }
+    }
+
+    fun bind(card: RecCard) {
         info.text = card.info
         users.text = card.users.toString()
         title.text = card.title
