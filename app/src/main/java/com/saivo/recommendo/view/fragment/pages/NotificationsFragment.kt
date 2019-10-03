2d707e19e86424bc9a3d6019516c1a84e38a9cfe
aaron <aaronnel.button@gmail.com>
2019-10-03 09:54:56 +0200

    added search function for notifications

--- app/src/main/java/com/saivo/recommendo/view/fragment/pages/NotificationsFragment.kt
+++ app/src/main/java/com/saivo/recommendo/view/fragment/pages/NotificationsFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.saivo.recommendo.R
 import com.saivo.recommendo.view.objects.RecyclerAdapter
 import com.saivo.recommendo.view.objects.notifications.NotifCard
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import kotlinx.android.synthetic.main.fragment_notifications.*
 
 /**
@@ -24,6 +29,7 @@ class NotificationsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
 
@@ -32,9 +38,19 @@ class NotificationsFragment : Fragment() {
         notifications_recycler_view.apply {
             layoutManager = LinearLayoutManager(this@NotificationsFragment.context)
             adapter = recyclerAdapter
+            recyclerAdapter.addToListItems(getData())
         }
 
-        recyclerAdapter.addToListItems(getData())
+        setupMaterialToolbar()
+
+
+    }
+
+    private fun setupMaterialToolbar() {
+        (activity as AppCompatActivity).apply {
+            setSupportActionBar(materialToolbar)
+            title = findNavController().currentDestination?.label
+        }
     }
 
     private fun getData(): ArrayList<NotifCard> {
@@ -47,6 +63,28 @@ class NotificationsFragment : Fragment() {
                 stars = 4.7,
                 users = 55,
                 image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBC7oKR9Y15uxvQjrkYNnFKFNaa-Rv_nSbQGfU5X3UrzOWirF2"
+            )
+
+        )
+        data.add(
+            NotifCard(
+                rated = "Recommended based off restaurant rating",
+                title = "Tiger's Milk",
+                rating ="4,1" ,
+                stars = 4.1,
+                users = 112,
+                image = "https://tigersmilk.co.za/wp-content/uploads/2018/03/001-2.jpg"
+            )
+
+        )
+        data.add(
+            NotifCard(
+                rated = "Recommended based off monument rating",
+                title = "Rhodes Memorial",
+                rating ="4,0" ,
+                stars = 4.0,
+                users = 150,
+                image = "https://media-cdn.tripadvisor.com/media/photo-s/0d/a3/90/71/view-from-rhodes-memorial.jpg"
             )
 
         )
